09b1ae9a1445ff2ff7c9601df2614574bc5efe7d
Aktar uz zaman <auzaman50.cse@gmail.com>
2024-04-03 00:04:26 +0600

    First Version

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+$dbHost = "localhost";
+$dbUser = "root";
+$dbPass = "";
+$dbName = "upload_image";
+$conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
+if (!$conn) {
+    die("something went wrong");
+}
+?>
